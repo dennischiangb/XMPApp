7ab1477c0f472fab400f9386606d105ca319eb99
Amec Velásquez <amec.vlsqz@gmail.com>
2017-10-13 10:24:14 -0600

    create command to show server information

---
+++ functions/commands/xmservers.js
@@ -0,0 +1,52 @@
+const lib = require('lib')({token: process.env.STDLIB_TOKEN});
+
+/**
+* /hello
+*
+*   Basic "Hello World" command.
+*   All Commands use this template, simply create additional files with
+*   different names to add commands.
+*
+*   See https://api.slack.com/slash-commands for more details.
+*
+* @param {string} user The user id of the user that invoked this command (name is usable as well)
+* @param {string} channel The channel id the command was executed in (name is usable as well)
+* @param {string} text The text contents of the command
+* @param {object} command The full Slack command object
+* @param {string} botToken The bot token for the Slack bot you have activated
+* @returns {object}
+*/
+module.exports = (user, channel, text = '', command = {}, botToken = null, callback) => {
+
+  var team = {
+    dennis: 'U0545PDQ3',
+    rodrigo: 'U0DU10LAU',
+    roger: 'U75RRPETH',
+    alejandro: 'U0408Q8R0',
+    amec: 'U707X17U3',
+    emily: 'U6XAQ854Y'
+  }
+
+
+  if(Object.values(team).indexOf(user) > -1){
+
+    if(!team[text]) {
+      callback(null, {
+        response_type: 'ephemeral',
+        text: `Hey <@${user}> here is the info for ${text}'s sever:`
+      });
+    } else {
+      callback(null, {
+        response_type: 'ephemeral',
+        text: `Sorry <@${user}> I don't know about the ${text} sever`
+      });
+    }
+  } else {
+    callback(null, {
+      response_type: 'ephemeral',
+      text: `Sorry, this command is exclusive to the XMPie team.`
+    });
+  }
+
+
+};
