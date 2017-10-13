d93059744cc8736db208506bd1ba36987edf4da6
dennischiangb <32773951+dennischiangb@users.noreply.github.com>
2017-10-13 10:11:49 -0600

    xmtrello

---
+++ functions/commands/xmtrello.js
@@ -0,0 +1,57 @@
+const lib = require('lib')({token: process.env.STDLIB_TOKEN});
+
+/**
+* /hello
+*
+*   Basic "Hello World" command.
+*   All Commands use this template, simply create additional files with
+*   different names to add commands.
+*
+*   See https://api.slack.com/slash-commands for more details.
+*
+* @param {string} user The user id of the user that invoked this command (name is usable as well)
+* @param {string} channel The channel id the command was executed in (name is usable as well)
+* @param {string} text The text contents of the command
+* @param {object} command The full Slack command object
+* @param {string} botToken The bot token for the Slack bot you have activated
+* @returns {object}
+*/
+module.exports = (user, channel, text = '', command = {}, botToken = null, callback) => {
+
+  if(channel == 'G76SVCPEV'){
+    callback(null, {
+      response_type: 'ephemeral',
+      "attachments": [
+        {
+            "fallback": "Required plain-text summary of the attachment.",
+            "color": "#36a64f",
+            "pretext": "Optional text that appears above the attachment block",
+            "author_name": "Bobby Tables",
+            "author_link": "http://flickr.com/bobby/",
+            "author_icon": "http://flickr.com/icons/bobby.jpg",
+            "title": "Slack API Documentation",
+            "title_link": "https://api.slack.com/",
+            "text": "Optional text that appears within the attachment",
+            "fields": [
+                {
+                    "title": "Priority",
+                    "value": "High",
+                    "short": false
+                }
+            ],
+            "image_url": "http://my-website.com/path/to/image.jpg",
+            "thumb_url": "http://example.com/path/to/thumb.png",
+            "footer": "Slack API",
+            "footer_icon": "https://platform.slack-edge.com/img/default_application_icon.png",
+            "ts": 123456789
+        }
+    ]});
+  }else{
+    callback(null, {
+      response_type: 'ephemeral',
+      text: `Sorry, this command is unsupported in this channel.`
+    });
+  }
+
+
+};
