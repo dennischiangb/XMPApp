bce59f12cdd498b47c8e47e2240f82e08290f49c
dennischiangb <32773951+dennischiangb@users.noreply.github.com>
2017-10-16 16:15:05 -0600

    sgfdsa

--- functions/commands/xmschedules.js
+++ functions/commands/xmschedules.js
@@ -66,7 +66,7 @@ module.exports = (user, channel, text = '', command = {}, botToken = null, callb
        ]
      }]
     }, (err, result) => {
-      callback(err {
+      callback(err, {
         response_type: 'ephemeral',
         text: `Ola ke iso`,
       });
